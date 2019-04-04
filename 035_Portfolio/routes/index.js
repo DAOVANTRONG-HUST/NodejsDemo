debb43f21f89ba11e8b5d20c6277bf1de17c7a0d
Đào Văn Trọng <daovantrong.hust@gmail.com>
2019-04-05 01:38:54 +0700

    header-footer

--- 035_Portfolio/routes/index.js
+++ 035_Portfolio/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/chi-tiet', function(req, res, next) {
+  res.render('chi-tiet-san-pham.ejs', { title: 'Trang chi tiet' });
+});
+
 module.exports = router;
