d2b76254f2501f81974e1ab6f73837ca0ba60ff0
Đào Văn Trọng <daovantrong.hust@gmail.com>
2019-04-05 14:47:18 +0700

    047 - Do du lieu ra trang chi tiet

--- 035_Portfolio/routes/index.js
+++ 035_Portfolio/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/chi-tiet/*.:idsanpham', function(req, res, next) {
+router.get('/*.:idsanpham', function(req, res, next) { // có thể sử dụng cách này để giải quyết 2 level mà không cần định nghĩa đường dẫn riêng
   res.render('chi-tiet-san-pham', {idsanpham: req.params.idsanpham });
 });
 
